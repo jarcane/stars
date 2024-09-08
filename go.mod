8951d51a80380ae069c770f785e73f018da54f42
Annaia Danvers <jarcane@users.noreply.github.com>
2024-09-08 09:14:04 -0500

    Add routes

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/jarcane/stars
 
 go 1.23.0
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/llgcode/draw2d v0.0.0-20240627062922-0ed1ff131195 // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	golang.org/x/image v0.20.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	"image/color"
 	"image/png"
 	"log"
+	"math"
 	"math/rand"
 	"os"
+
+	"github.com/llgcode/draw2d/draw2dimg"
+	"github.com/samber/lo"
 )
 
 const (
@@ -15,6 +19,7 @@ const (
 	Y_SIZE           = 480
 	MIN_STARS        = 50
 	MAX_STARS        = 150
+	MAX_RANGE        = 100
 	DEFAULT_FILENAME = "stars.png"
 )
 
@@ -24,8 +29,8 @@ type Star struct {
 	class SpectralClass
 }
 
-func create_blank(max_x int, max_y int) *image.NRGBA {
-	img := image.NewNRGBA(image.Rect(0, 0, max_x, max_y))
+func create_blank(max_x int, max_y int) *image.RGBA {
+	img := image.NewRGBA(image.Rect(0, 0, max_x, max_y))
 
 	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
 		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
@@ -47,7 +52,7 @@ func generate_stars(max_x int, max_y int) []Star {
 	return stars
 }
 
-func draw_stars(img *image.NRGBA, stars []Star) {
+func draw_stars(img *image.RGBA, stars []Star) {
 	for n := 0; n < len(stars); n++ {
 		star := stars[n]
 		x := star.x
@@ -62,7 +67,40 @@ func draw_stars(img *image.NRGBA, stars []Star) {
 	}
 }
 
-func write_image(img *image.NRGBA, filename string) {
+func is_habitable(s Star) bool {
+	return s.class == G || s.class == K || s.class == M
+}
+
+func is_in_range(orig Star, dest Star) bool {
+	x_diff := math.Pow(float64(orig.x-dest.x), 2)
+	y_diff := math.Pow(float64(orig.y-dest.y), 2)
+	dist := math.Sqrt(x_diff + y_diff)
+
+	return dist <= MAX_RANGE
+}
+
+func draw_routes(img *image.RGBA, stars []Star) {
+	habitable := lo.Filter(stars, func(s Star, _ int) bool { return is_habitable(s) })
+
+	gc := draw2dimg.NewGraphicContext(img)
+
+	gc.SetFillColor(color.RGBA{128, 128, 128, 255})
+	gc.SetStrokeColor(color.RGBA{128, 128, 128, 255})
+	gc.SetLineWidth(0.2)
+
+	for _, s := range habitable {
+		in_range := lo.Filter(habitable, func(dest Star, _ int) bool { return is_in_range(s, dest) })
+
+		for _, d := range in_range {
+			gc.MoveTo(float64(s.x), float64(s.y))
+			gc.LineTo(float64(d.x), float64(d.y))
+			gc.Close()
+			gc.FillStroke()
+		}
+	}
+}
+
+func write_image(img *image.RGBA, filename string) {
 	f, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -88,7 +126,7 @@ func main() {
 
 	stars := generate_stars(*x, *y)
 
+	draw_routes(img, stars)
 	draw_stars(img, stars)
-
 	write_image(img, *filename)
 }
