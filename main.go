6903d2a323cda2db85f77cc95944aba798878463
Annaia Danvers <jarcane@users.noreply.github.com>
2024-09-07 22:52:33 -0500

    Draw a rectangle!

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
+)
+
 func main() {
-	println("Hello, World!")
+	stars := image.NewNRGBA(image.Rect(0, 0, 640, 480))
+
+	for y := stars.Bounds().Min.Y; y < stars.Bounds().Max.Y; y++ {
+		for x := stars.Bounds().Min.X; x < stars.Bounds().Max.X; x++ {
+			stars.Set(x, y, color.Black)
+		}
+	}
+
+	f, err := os.Create("stars.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := png.Encode(f, stars); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
