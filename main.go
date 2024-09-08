451f7d503ca58b893df9ed00843c9c6417bfe43f
Annaia Danvers <jarcane@users.noreply.github.com>
2024-09-08 06:55:55 -0500

    Refactor and set min # of stars

--- main.go
+++ main.go
@@ -9,27 +9,39 @@ import (
 	"os"
 )
 
-func main() {
-	stars := image.NewNRGBA(image.Rect(0, 0, 640, 480))
+type Star struct {
+	x     int
+	y     int
+	class SpectralClass
+}
 
-	for y := stars.Bounds().Min.Y; y < stars.Bounds().Max.Y; y++ {
-		for x := stars.Bounds().Min.X; x < stars.Bounds().Max.X; x++ {
-			stars.Set(x, y, color.Black)
+func main() {
+	img := image.NewNRGBA(image.Rect(0, 0, 640, 480))
+
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			img.Set(x, y, color.Black)
 		}
 	}
 
-	num_stars := rand.Intn(150)
+	num_stars := 50 + rand.Intn(100)
+	stars := make([]Star, num_stars)
 
 	for n := 0; n < num_stars; n++ {
-		x := rand.Intn(640)
-		y := rand.Intn(480)
-		c := spectralClasses[SpectralClass(rand.Intn(7))].toRGBA(255)
+		stars[n] = Star{rand.Intn(640), rand.Intn(480), SpectralClass(rand.Intn(7))}
+	}
 
-		stars.Set(x, y, c)
-		stars.Set(x+1, y, c)
-		stars.Set(x, y+1, c)
-		stars.Set(x-1, y, c)
-		stars.Set(x, y-1, c)
+	for n := 0; n < num_stars; n++ {
+		star := stars[n]
+		x := star.x
+		y := star.y
+		c := spectralClasses[star.class].toRGBA(255)
+
+		img.Set(x, y, c)
+		img.Set(x+1, y, c)
+		img.Set(x, y+1, c)
+		img.Set(x-1, y, c)
+		img.Set(x, y-1, c)
 	}
 
 	f, err := os.Create("stars.png")
@@ -37,7 +49,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := png.Encode(f, stars); err != nil {
+	if err := png.Encode(f, img); err != nil {
 		f.Close()
 		log.Fatal(err)
 	}
